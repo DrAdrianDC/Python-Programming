8f9a7dbc5007bcbab3026b8daed81581e87af39c
Adrian Dominguez-Castro <157868503+DrAdrianDC@users.noreply.github.com>
2024-10-22 07:06:35 -0700

    Add files via upload

---
+++ Plotting_using_Python/candlestick.py
@@ -0,0 +1,1 @@
+# Candlestick Chart Plot using Pythonimport yfinance  as yfimport mplfinance as mpfticker = input("Enter the stock name: ")df = yf.download(ticker, start='2023-08-01' , end='2024-09-01')mpf.plot(df, type='candle', style='charles' ,                title=f'{ticker} Candlestick Chart' , ylabel='Price',                savefig=f'{ticker}_candlestick_chart.png')
